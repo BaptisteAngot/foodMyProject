0f3860f035aead22c4d47a452d074bfb1765c063
Baptiste Angot <bat76640@gmail.com>
2022-06-08 15:39:25 +0200

    add donation API

--- app/src/main/java/com/nfs/foodmyproject/config/ApiEndpoint.java
+++ app/src/main/java/com/nfs/foodmyproject/config/ApiEndpoint.java
@@ -1,8 +1,9 @@
 package com.nfs.foodmyproject.config;
 
 public class ApiEndpoint {
-    public static String BASE = "http://192.168.1.12:8000/api/";
+    public static String BASE = "http://172.26.80.1:8000/api/";
     public static String GET_PROJECTS = "project";
     public static String GET_PALIERS = "tier/";
+    public static String ADD_DONATIONS = "donation/add";
 
 }
