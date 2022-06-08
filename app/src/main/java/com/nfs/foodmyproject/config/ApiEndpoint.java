943580d4e0e08a0ed8e4462550b9d7548e4b1927
Baptiste Angot <bat76640@gmail.com>
2022-06-08 10:33:54 +0200

    Add config file for API url

---
+++ app/src/main/java/com/nfs/foodmyproject/config/ApiEndpoint.java
@@ -0,0 +1,7 @@
+package com.nfs.foodmyproject.config;
+
+public class ApiEndpoint {
+    public static String BASE = "http://172.26.80.1:8000/api/";
+    public static String GET_PROJECTS = "project";
+
+}
